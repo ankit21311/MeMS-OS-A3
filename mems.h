bb26461765c892fafc74e71cb3f65b83562fc389
Ankit Raj <108571573+ankit21311@users.noreply.github.com>
2023-11-06 14:06:47 +0530

    Debuggging

--- mems.h
+++ mems.h
@@ -1,143 +1,106 @@
 /*
+
 All the main functions with respect to the MeMS are inplemented here
+
 read the function discription for more details
 
+
+
 NOTE: DO NOT CHANGE THE NAME OR SIGNATURE OF FUNCTIONS ALREADY PROVIDED
-you are only allowed to implement the functions 
+
+you are only allowed to implement the functions
+
 you can also make additional helper functions a you wish
 
-REFER DOCUMENTATION FOR MORE DETAILS ON FUNSTIONS AND THEIR FUNCTIONALITY
+
+
+REFER DOCUMENTATION FOR MORE DETAILS ON FUNCTIONS AND THEIR FUNCTIONALITY
+
 */
+
+
+
 // add other headers as required
-#include<stdio.h>
-#include<stdlib.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <sys/mman.h>
+
 
 
 /*
+
 Use this macro where ever you need PAGE_SIZE.
-As PAGESIZE can differ system to system we should have flexibility to modify this 
-macro to make the output of all system same and conduct a fair evaluation. 
+
+As PAGESIZE can differ system to system we should have flexibility to modify this
+
+macro to make the output of all system same and conduct a fair evaluation.
+
 */
+
 #define PAGE_SIZE 4096
 
 
-/*
-Initializes all the required parameters for the MeMS system. The main parameters to be initialized are:
-1. the head of the free list i.e. the pointer that points to the head of the free list
-2. the starting MeMS virtual address from which the heap in our MeMS virtual address space will start.
-3. any other global variable that you want for the MeMS implementation can be initialized here.
-Input Parameter: Nothing
-Returns: Nothing
-*/
 
-// Making main chain
-struct MainChainNode {
-    int memory_size;
-    SubChainNode *sub_chain;
-    struct MainChainNode* next;
-    struct MainChainNode* prev;
-};
+// Define a flag for anonymous mapping
 
-
-struct SubChainNode {
-
-    void* address;
-
-    size_t size;
-
-    int is_mapped;
-
-    struct SubChainNode *prev;
-
-    struct SubChainNode *next;
-
-};
-
-// mems init
-void mems_init() {
-
-    mems_system = (MeMS*)mems_memory;
-
-    mems_system->free_list = NULL;
-
-    mems_system->mem_start = &mems_memory[sizeof(MeMS)];
-
-    mems_system->used_pages = 0;
-
-    mems_system->unused_memory = MEMS_MEMORY_SIZE - sizeof(MeMS);
-
-}
+#define MAP_ANONYMOUS 0x20
 
 
 
-/*
-This function will be called at the end of the MeMS system and its main job is to unmap the 
-allocated memory using the munmap system call.
-Input Parameter: Nothing
-Returns: Nothing
-*/
-void mems_finish() {
+// global variables
 
-    // No need to unmap, as we're not using mmap
+struct chainNode *memoryChainHead;
 
-    // Just reset the MeMS structure and free_list
+size_t virtualAddressStart;
 
-    mems_system->free_list = NULL;
 
-    mems_system->mem_start = NULL;
 
-    mems_system->used_pages = 0;
+/* ------------------- mmap and munmap ------------------- */
 
-    mems_system->unused_memory = 0;
+
+
+void *allocate_memory_mmap(size_t size)
+
+{ // Checking if size is valid
+
+      if (size == 0) {
+
+        printf("Invalid memory allocation size: 0\n");
+
+        return NULL;
+
+    }
+
+
+
+    // Calculate aligned memory size to match page boundaries
+
+    size_t alignedSize = (size + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+
+
+
+    // Allocate aligned memory using mmap
+
+    void *allocatedMemory = mmap(NULL, alignedSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (allocatedMemory == MAP_FAILED) {
+
+        printf("Error allocating memory using mmap\n");
+
+        return NULL;
+
+    }
+
+
+
+    // Return allocated memory pointer
+
+    return allocatedMemory;
 
 }
 
 
-/*
-Allocates memory of the specified size by reusing a segment from the free list if 
-a sufficiently large segment is available. 
 
-Else, uses the mmap system call to allocate more memory on the heap and updates 
-the free list accordingly.
-
-Note that while mapping using mmap do not forget to reuse the unused space from mapping
-by adding it to the free list.
-Parameter: The size of the memory the user program wants
-Returns: MeMS Virtual address (that is created by MeMS)
-*/ 
-void* mems_malloc(size_t size){
-
-}
-
-
-/*
-this function print the stats of the MeMS system like
-1. How many pages are utilised by using the mems_malloc
-2. how much memory is unused i.e. the memory that is in freelist and is not used.
-3. It also prints details about each node in the main chain and each segment (PROCESS or HOLE) in the sub-chain.
-Parameter: Nothing
-Returns: Nothing but should print the necessary information on STDOUT
-*/
-void mems_print_stats(){
-
-}
-
-
-/*
-Returns the MeMS physical address mapped to ptr ( ptr is MeMS virtual address).
-Parameter: MeMS Virtual address (that is created by MeMS)
-Returns: MeMS physical address mapped to the passed ptr (MeMS virtual address).
-*/
-void *mems_get(void*v_ptr){
-    
-}
-
-
-/*
-this function free up the memory pointed by our virtual_address and add it to the free list
-Parameter: MeMS Virtual address (that is created by MeMS) 
-Returns: nothing
-*/
-void mems_free(void *v_ptr){
-    
-}
